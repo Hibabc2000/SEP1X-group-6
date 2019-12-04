403308476adc0dc96fbb3688bbc5d92ace60cc6f
Marin Bilba <39630044+marinbilba@users.noreply.github.com>
2019-12-04 14:04:48 +0100

    Class and Course

---
+++ SEP/src/Class.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Class
+{
+  private String className;
+  private ArrayList<Student> students;
+
+  public Class(String className, ArrayList<Student> students)
+  {
+    this.className = className;
+    students = new ArrayList<Student>();
+  }
+
+  public ArrayList<Student> getAllStudentsOfClass(String className)
+  {
+
+  }
+}
