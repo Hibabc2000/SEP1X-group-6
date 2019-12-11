d173ed12281c09db1ba1b2d82137597a3e5e36c4
GitGudNubHub <58514021+GitGudNubHub@users.noreply.github.com>
2019-12-11 12:44:11 +0100

    Changes to RoomController

--- SEP/src/RoomController.java
+++ SEP/src/RoomController.java
@@ -12,8 +12,170 @@ import javafx.collections.FXCollections;
 
     import java.util.ArrayList;
 
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+/**
+ * @author Kristóf Lénárd, 293110
+ * @version v1.0
+ * A class for controlling the common GUI elements.
+ */
+
 public class RoomController implements EventHandler<ActionEvent>
-{/**
+{
+  @FXML private Button homeButton;
+  @FXML private Button roomButton;
+  @FXML private Button teacherButton;
+  @FXML private Button coExaminerButton;
+  @FXML private Button courseButton;
+  @FXML private Button scheduleButton;
+  @FXML private Button settingsButton;
+  private Scene scene;
+  private Stage stage;
+
+  public RoomController()
+  {
+  }
+
+  @Override public void handle(ActionEvent actionEvent)
+  {
+    if (actionEvent.getSource() == homeButton)
+    {
+      try
+      {
+        changeScene("Home.fxml", actionEvent);
+      }
+      catch (IOException e)
+      {
+        e.printStackTrace();
+        System.exit(1);
+      }
+      Alert alert = new Alert(Alert.AlertType.INFORMATION, "Successful load on Home!");
+      alert.showAndWait();
+    }
+
+    if (actionEvent.getSource().equals(roomButton))
+    {
+      try
+      {
+        changeScene("Rooms.fxml", actionEvent);
+      }
+      catch (IOException e)
+      {
+        e.printStackTrace();
+        System.exit(1);
+      }
+    }
+    if (actionEvent.getSource().equals(teacherButton))
+    {
+      try
+      {
+        FXMLLoader.load(getClass().getResource("Teacher.fxml"));
+        System.out.println("Successful load");
+      }
+      catch (IOException e)
+      {
+        e.printStackTrace();
+        System.exit(1);
+      }
+    }
+    if (actionEvent.getSource().equals(coExaminerButton))
+    {
+      try
+      {
+        changeScene("Co-examiner.fxml", actionEvent);
+      }
+      catch (IOException e)
+      {
+        e.printStackTrace();
+        System.exit(1);
+      }
+    }
+    if (actionEvent.getSource().equals(courseButton))
+    {
+      try
+      {
+        changeScene("Course.fxml", actionEvent);
+      }
+      catch (IOException e)
+      {
+        e.printStackTrace();
+        System.exit(1);
+      }
+    }
+    if (actionEvent.getSource().equals(scheduleButton))
+    {
+      try
+      {
+        changeScene("addUpdateSchedule.fxml", actionEvent);
+      }
+      catch (IOException e)
+      {
+        e.printStackTrace();
+        System.exit(1);
+      }
+    }
+    if (actionEvent.getSource().equals(settingsButton))
+    {
+      try
+      {
+        changeScene("Settings.fxml", actionEvent);
+      }
+      catch (IOException e)
+      {
+        e.printStackTrace();
+        System.exit(1);
+      }
+    }
+  }
+
+  private void changeScene(String target, ActionEvent event) throws IOException
+  {
+    Parent parent = FXMLLoader.load(getClass().getResource("Rooms.fxml"));
+    Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    stage.getScene().setRoot(parent);
+    stage.show();
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
  *@author Oliver Isaac, 293131
  *@version 1.0.0.0
  *this class is just a conTROLLer class for my Room UI.
@@ -62,8 +224,5 @@ public class RoomController implements EventHandler<ActionEvent>
     dota.setItems(FXCollections.observableArrayList(list2));
     dota.show();
   }
-  @Override public void handle(ActionEvent actionEvent)
-  {
-    //starting();
-  }
+
 }
