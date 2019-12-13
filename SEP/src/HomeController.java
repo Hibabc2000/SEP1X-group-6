9d7d8472e4ec4c6ea6944039cbd4c71a6ead8a6d
Kristóf Lénárd <293110@via.dk>
2019-12-13 13:25:22 +0100

    Finished (mostly) HomeController

--- SEP/src/HomeController.java
+++ SEP/src/HomeController.java
@@ -1,3 +1,4 @@
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -14,126 +15,131 @@ import java.util.ArrayList;
 
 public class HomeController implements EventHandler<ActionEvent>
 {
-    @FXML private TableView<Exam> tableView;
-    @FXML private TableColumn Week;
-    @FXML private TableColumn Date;
-    @FXML private TableColumn Course;
-    @FXML private TableColumn NumStud;
-    @FXML private TableColumn Type;
-    @FXML private TableColumn Time;
-    @FXML private TableColumn Room;
-    @FXML private TableColumn Teacher;
-    @FXML private Button homeButton;
-    @FXML private Button roomButton;
-    @FXML private Button teacherButton;
-    @FXML private Button coExaminerButton;
-    @FXML private Button courseButton;
-    @FXML private Button scheduleButton;
-    @FXML private Button settingsButton;
-    private ArrayList<Exam> exams;
+  @FXML private TableView tableView;
+  @FXML private TableColumn Week;
+  @FXML private TableColumn Date;
+  @FXML private TableColumn Course;
+  @FXML private TableColumn NumStud;
+  @FXML private TableColumn Type;
+  @FXML private TableColumn Time;
+  @FXML private TableColumn Room;
+  @FXML private TableColumn Teacher;
+  @FXML private Button homeButton;
+  @FXML private Button roomButton;
+  @FXML private Button teacherButton;
+  @FXML private Button coExaminerButton;
+  @FXML private Button courseButton;
+  @FXML private Button scheduleButton;
+  @FXML private Button settingsButton;
+  private ArrayList<Exam> exams;
 
-    public HomeController()
+  public HomeController()
+  {
+    exams = new ArrayList<Exam>();
+  }
+
+  @Override public void handle(ActionEvent actionEvent)
+  {
+    if (actionEvent.getSource() == homeButton)
     {
-      //exams = ExamList.getAllExams();
-      //tableView.getItems().addAll(exams);
+      try
+      {
+        changeScene("Home.fxml", actionEvent);
+      }
+      catch (IOException e)
+      {
+        e.printStackTrace();
+        System.exit(1);
+      }
+      Alert alert = new Alert(Alert.AlertType.INFORMATION,
+          "Successful load on Home!");
+      alert.showAndWait();
     }
 
-    @Override public void handle(ActionEvent actionEvent)
+    if (actionEvent.getSource().equals(roomButton))
     {
-        if (actionEvent.getSource() == homeButton)
-        {
-            try
-            {
-                changeScene("Home.fxml", actionEvent);
-            }
-            catch (IOException e)
-            {
-                e.printStackTrace();
-                System.exit(1);
-            }
-            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Successful load on Home!");
-            alert.showAndWait();
-        }
-
-        if (actionEvent.getSource().equals(roomButton))
-        {
-            try
-            {
-                changeScene("Rooms.fxml", actionEvent);
-            }
-            catch (IOException e)
-            {
-                e.printStackTrace();
-                System.exit(1);
-            }
-        }
-        if (actionEvent.getSource().equals(teacherButton))
-        {
-            try
-            {
-                changeScene("Teacher.fxml", actionEvent);
-            }
-            catch (IOException e)
-            {
-                e.printStackTrace();
-                System.exit(1);
-            }
-        }
-        if (actionEvent.getSource().equals(coExaminerButton))
-        {
-            try
-            {
-                changeScene("Co-examiner.fxml", actionEvent);
-            }
-            catch (IOException e)
-            {
-                e.printStackTrace();
-                System.exit(1);
-            }
-        }
-        if (actionEvent.getSource().equals(courseButton))
-        {
-            try
-            {
-                changeScene("addUpdateCourse.fxml", actionEvent);
-            }
-            catch (IOException e)
-            {
-                e.printStackTrace();
-                System.exit(1);
-            }
-        }
-        if (actionEvent.getSource().equals(scheduleButton))
-        {
-            try
-            {
-                changeScene("addUpdateSchedule.fxml", actionEvent);
-            }
-            catch (IOException e)
-            {
-                e.printStackTrace();
-                System.exit(1);
-            }
-        }
-        if (actionEvent.getSource().equals(settingsButton))
-        {
-            try
-            {
-                changeScene("Settings.fxml", actionEvent);
-            }
-            catch (IOException e)
-            {
-                e.printStackTrace();
-                System.exit(1);
-            }
-        }
+      try
+      {
+        changeScene("Rooms.fxml", actionEvent);
+      }
+      catch (IOException e)
+      {
+        e.printStackTrace();
+        System.exit(1);
+      }
     }
-
-    private void changeScene(String target, ActionEvent event) throws IOException
+    if (actionEvent.getSource().equals(teacherButton))
     {
-        Parent parent = FXMLLoader.load(getClass().getResource(target));
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.getScene().setRoot(parent);
-        stage.show();
+      try
+      {
+        changeScene("Teacher.fxml", actionEvent);
+      }
+      catch (IOException e)
+      {
+        e.printStackTrace();
+        System.exit(1);
+      }
     }
+    if (actionEvent.getSource().equals(coExaminerButton))
+    {
+      try
+      {
+        changeScene("Co-examiner.fxml", actionEvent);
+      }
+      catch (IOException e)
+      {
+        e.printStackTrace();
+        System.exit(1);
+      }
+    }
+    if (actionEvent.getSource().equals(courseButton))
+    {
+      try
+      {
+        changeScene("addUpdateCourse.fxml", actionEvent);
+      }
+      catch (IOException e)
+      {
+        e.printStackTrace();
+        System.exit(1);
+      }
+    }
+    if (actionEvent.getSource().equals(scheduleButton))
+    {
+      try
+      {
+        changeScene("addUpdateSchedule.fxml", actionEvent);
+      }
+      catch (IOException e)
+      {
+        e.printStackTrace();
+        System.exit(1);
+      }
+    }
+    if (actionEvent.getSource().equals(settingsButton))
+    {
+      try
+      {
+        changeScene("Settings.fxml", actionEvent);
+      }
+      catch (IOException e)
+      {
+        e.printStackTrace();
+        System.exit(1);
+      }
+    }
+  }
+
+  private void changeScene(String target, ActionEvent event) throws IOException
+  {
+    Parent parent = FXMLLoader.load(getClass().getResource(target));
+    Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    stage.getScene().setRoot(parent);
+    stage.show();
+  }
+
+  private void tableUpdate()
+  {
+    tableView.setItems(FXCollections.observableList(exams));
+  }
 }
