0e6f61869ba144dcead2ce777596648c616a5cca
GitGudNubHub <58514021+GitGudNubHub@users.noreply.github.com>
2019-12-13 11:23:20 +0100

    RoomController  updated

--- SEP/src/Home.java
+++ SEP/src/Home.java
@@ -3,40 +3,48 @@ import java.util.ArrayList;
 public class Home
 {
 
-    public byte getDay(Exam exam)
-    {
-        return exam.getDate().getDay();
-    }
-    public byte getStartHour(Exam exam)
-    {
-        return exam.getDate().getStartHour();
-    }
-    public byte getEndHour(Exam exam)
-    {
-        return exam.getDate().getEndHour();
-    }
-    public byte getWeek(Exam exam)
-    {
-        return exam.getDate().getWeek();
-    }
-    public String getCourseName(Exam exam)
-    {
-        return exam.getCourse().getCourseName();
-    }
-    public int getStudentsNumber(Exam exam)
-    {
-        return exam.getCourse().getNrStudents();
-    }
-    public int getSemester(Exam exam)
-    {
-        return exam.getCourse().getSemester();
-    }
-    public String getRoomNumber(Exam exam)
-    {
-        return exam.getRoom().getRoomNumber();
-    }
-    public String getTeacherName(Exam exam)
-    {
-        return exam.getTeacher().getName();
-    }
+  public byte getDay(Exam exam)
+  {
+    return exam.getDate().getDay();
+  }
+
+  public byte getStartHour(Exam exam)
+  {
+    return exam.getDate().getStartHour();
+  }
+
+  public byte getEndHour(Exam exam)
+  {
+    return exam.getDate().getEndHour();
+  }
+
+  public byte getWeek(Exam exam)
+  {
+    return exam.getDate().getWeek();
+  }
+
+  public String getCourseName(Exam exam)
+  {
+    return exam.getCourse().getCourseName();
+  }
+
+  public int getStudentsNumber(Exam exam)
+  {
+    return exam.getCourse().getNrStudents();
+  }
+
+  public int getSemester(Exam exam)
+  {
+    return exam.getCourse().getSemester();
+  }
+
+  public String getRoomNumber(Exam exam)
+  {
+    return exam.getRoom().getRoomNumber();
+  }
+
+  public String getTeacherName(Exam exam)
+  {
+    return exam.getTeacher().getName();
+  }
 }
